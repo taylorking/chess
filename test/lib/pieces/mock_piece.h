e3248bc16c00ae55ce2ed80816b5826e9bc196a6
Taylor King <taylor@taylorking.io>
2023-05-19 20:29:42 -0400

    Clang tidy fixes

--- test/lib/pieces/mock_piece.h
+++ test/lib/pieces/mock_piece.h
@@ -3,7 +3,7 @@ namespace chess {
 class MockPiece : public Piece {
   public:
     MockPiece(Color color): Piece(color, -1, -1) {}
-    std::string GetName() const override {
+    std::string GetName() const {
       return "dummy";
     }
     std::vector<Move> GetRawMoves(const ChessBoard& board) const override {
